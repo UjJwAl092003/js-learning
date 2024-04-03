108b6fdea3a77a73e407d68ea87427293f3f8116
Ujjwal Singh <99257263+UjJwAl092003@users.noreply.github.com>
2024-04-03 16:37:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+
+//alert(10 + 2) //we are using node js , not browser
+
+let name="ujjwal"
+let age=23
+let isLoggedIn=false
+
+//number=>2 to power 53
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol==>unique
+
+
+//object
+
+console.log(typeof "ujjwal");
+console.log(typeof null)
+console.log(typeof undefined)
